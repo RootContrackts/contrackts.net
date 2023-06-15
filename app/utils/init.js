1f129568840d28365ee05345824bc92fc03aadb8
RootContrackts <136607294+RootContrackts@users.noreply.github.com>
2023-06-15 15:49:35 +0530

    Update init.js

--- app/utils/init.js
+++ app/utils/init.js
@@ -2,10 +2,15 @@ import contracktV001 from "../abi/contracktV001.json";
 import { ethers } from "ethers";
 
 export async function init() {
-  const { abi } = contracktV001;
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contractAddress = "0xe83D13b456F9F6BD26f50a9290dBB0c7eACF1EBe";
-  const contrackt = new ethers.Contract(contractAddress, abi, signer);
-  return contrackt;
+  if (typeof window !== "undefined" && window.ethereum) {
+    const { abi } = contracktV001;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractAddress = "0xe83D13b456F9F6BD26f50a9290dBB0c7eACF1EBe";
+    const contrackt = new ethers.Contract(contractAddress, abi, signer);
+    return contrackt;
+  } else {
+    throw new Error("This code must be executed in a browser environment with MetaMask installed.");
+  }
 }
